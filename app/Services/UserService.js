873ca44991d335553d1069e88236cd571ba45ba3
Javier Rodríguez <jrolopez20@gmail.com>
2020-10-29 09:44:15 -0400

    Allow to manage user address

--- app/Services/UserService.js
+++ app/Services/UserService.js
@@ -1,6 +1,8 @@
 'use strict'
 
+const Address = use('App/Models/Address');
 const User = use('App/Models/User');
+const Database = use('Database');
 const Drive = use('Drive');
 const Helpers = use('Helpers');
 const BadRequestException = use('App/Exceptions/BadRequestException');
@@ -10,7 +12,7 @@ class UserService {
 
     static async addUser(request) {
         const {
-            email, password, repassword, numid, fullname, telephone, address, officeId, role
+            email, password, repassword, numid, fullname, telephone, address, office, role, picture
         } = request.all()
 
         const givenUser = await User.query().where('email', email).first();
@@ -23,43 +25,101 @@ class UserService {
             throw new BadRequestException('Password do not match.');
         }
 
-        let user = new User();
-        user.email = email;
-        user.password = password
-        user.numid = numid;
-        user.fullname = fullname;
-        user.telephone = telephone;
-        user.address = address;
-        user.officeId = officeId;
-        user.role = role || User.roles().CLIENT;
+        const trx = await Database.beginTransaction()
+        try {
+            let user = new User();
+            user.email = email;
+            user.password = password
+            user.numid = numid;
+            user.fullname = fullname;
+            user.telephone = telephone;
+            user.picture = picture;
+            if (office) {
+                user.officeId = office.id;
+            }
+            user.role = role || User.roles().CLIENT;
 
-        await user.save();
+            await user.save(trx);
 
-        return User.find(user.id);
+            if (address) {
+                let addressObj = new Address();
+                addressObj.localidadId = address.localidad.id
+                addressObj.description = address.description
+                addressObj.coordinates = address.coordinates
+                await addressObj.save(trx);
+
+                await user.address().associate(addressObj, trx)
+            }
+
+            // End transaction
+            await trx.commit();
+            await user.load('address');
+            await user.load('office.provincia');
+            return user;
+        } catch (e) {
+            console.log(e)
+            trx.rollback();
+            throw new Error(e.message)
+        }
     }
 
     static async setUser(userId, request) {
+        const email = request.input("email")
         const numid = request.input("numid")
         const fullname = request.input("fullname")
         const telephone = request.input("telephone")
         const address = request.input("address")
-        const officeId = request.input("officeId")
+        const picture = request.input("picture")
+        const office = request.input("office")
         let role = request.input("role")
 
-        let user = await User.find(userId)
-        user.numid = numid;
-        user.fullname = fullname;
-        user.telephone = telephone;
-        user.address = address;
-        user.officeId = officeId;
+        const trx = await Database.beginTransaction()
+        try {
+            let user = await User.find(userId)
+            user.email = email;
+            user.numid = numid;
+            user.fullname = fullname;
+            user.telephone = telephone;
 
-        if (role) {
-            user.role = role;
+            if (picture) {
+                user.picture = picture;
+            }
+
+            if(office) {
+                user.officeId = office.id;
+            }
+
+            if (role) {
+                user.role = role;
+            }
+
+            // Edit address
+            if (address) {
+                await user.load('address');
+                const addressObj = await user.getRelated('address');
+
+                if (address.localidad) {
+                    addressObj.localidadId = address.localidad.id;
+                }
+                if (address.description) {
+                    addressObj.description = address.description;
+                }
+
+                await addressObj.save(trx)
+            }
+
+            await user.save(trx);
+
+            // End transaction
+            await trx.commit();
+
+            await user.load('office.provincia');
+            return user;
+        } catch (e) {
+            console.log(e)
+            trx.rollback();
+            throw new Error(e.message)
         }
-
-        await user.save();
-
-        return user;
     }
 
     static async destroyUser(userId, auth) {
