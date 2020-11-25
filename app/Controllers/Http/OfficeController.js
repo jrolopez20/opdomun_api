ba353464168bcb2007afc4a8b17f936a07bb5fe4
Javier Rodríguez <jrolopez20@gmail.com>
2020-11-25 14:09:07 -0500

    Add endpoint to show a single office by ID

--- app/Controllers/Http/OfficeController.js
+++ app/Controllers/Http/OfficeController.js
@@ -6,6 +6,7 @@
 
 const Office = use('App/Models/Office')
 const PaginatedResponse = use('App/Util/PaginatedResponse');
+const ResourceNotFoundException = use('App/Exceptions/ResourceNotFoundException');
 
 /**
  * Resourceful controller for interacting with offices
@@ -26,6 +27,23 @@ class OfficeController {
         const limit = request.input('limit');
         const result = await Office.getOffices(page, limit);
         return PaginatedResponse.parse(response, result)
+    }
+
+    /**
+     * Display a single office.
+     * GET offices/:id
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
+    async show({params, request, response}) {
+        try {
+            const office = await Office.getOffice(params.id)
+            return response.json(office)
+        } catch (e) {
+            throw new ResourceNotFoundException();
+        }
     }
 
 }
