ab66894033ae31c53582eb3355c0335928145eaf
Javier Rodríguez <jrolopez20@gmail.com>
2020-12-04 11:56:46 -0500

    Update PaginatedResponse

--- app/Util/PaginatedResponse.js
+++ app/Util/PaginatedResponse.js
@@ -3,10 +3,19 @@
 class PaginatedResponse {
 
     static parse(response, result) {
-        response.header('Pagination-Count', result.pagination.total)
-        response.header('Pagination-Page', result.pagination.page)
-        response.header('Pagination-Limit', result.pagination.perPage)
-        response.header('Access-Control-Expose-Headers', 'Pagination-Count, Pagination-Page, Pagination-Limit')
+        console.log(result)
+        if (result.pagination) {
+            response.header('Pagination-Count', result.pagination.total)
+            response.header('Pagination-Page', result.pagination.page)
+            response.header('Pagination-Limit', result.pagination.perPage)
+            response.header('Access-Control-Expose-Headers', 'Pagination-Count, Pagination-Page, Pagination-Limit')
+        } else {
+            response.header('Pagination-Count', result.total)
+            response.header('Pagination-Page', result.page)
+            response.header('Pagination-Limit', result.perPage)
+            response.header('Access-Control-Expose-Headers', 'Pagination-Count, Pagination-Page, Pagination-Limit')
+        }
+
 
         return response.json(result.data)
     }
