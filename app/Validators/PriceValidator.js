f63cc7d8bb5979ce81d44227196abddff22e648a
Javier Rodríguez <jrolopez20@gmail.com>
2021-01-10 22:49:09 -0500

    Restrict min price

--- app/Validators/PriceValidator.js
+++ app/Validators/PriceValidator.js
@@ -19,6 +19,16 @@ const checkPrice = async (data, field, message, args, get) => {
         if (!CurrencyService.CURRENCIES()[data[field].currency.toUpperCase()]) {
             throw `The currency ${data[field].currency} is not supported`
         }
+
+        // Validate min value
+        const valueCUP = CurrencyService.transform(
+            data[field].value,
+            data[field].currency,
+            CurrencyService.CURRENCIES().CUP
+        )
+        if (valueCUP < 25000) {
+            throw 'El precio debe ser mayor que 25000 CUP o su equivalente en USD'
+        }
     }
 }
 Validator.extend('price', checkPrice);
